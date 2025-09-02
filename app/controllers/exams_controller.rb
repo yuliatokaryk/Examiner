b43cc56c0055576472ec25ad4d5ae13f17dfe5f2
yuliatokaryk <julia4099@gmail.com>
2025-09-02 11:18:59 -0600

    Edit Exam controller and view

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,69 +1,48 @@
 class ExamsController < ApplicationController
   before_action :set_exam, only: %i[ show edit update destroy ]
 
-  # GET /exams or /exams.json
   def index
     @exams = Exam.all
   end
 
-  # GET /exams/1 or /exams/1.json
   def show
   end
 
-  # GET /exams/new
   def new
     @exam = Exam.new
   end
 
-  # GET /exams/1/edit
   def edit
   end
 
-  # POST /exams or /exams.json
   def create
     @exam = Exam.new(exam_params)
 
-    respond_to do |format|
-      if @exam.save
-        format.html { redirect_to @exam, notice: "Exam was successfully created." }
-        format.json { render :show, status: :created, location: @exam }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @exam.errors, status: :unprocessable_entity }
-      end
+    if @exam.save
+      redirect_to @exam, notice: "Exam was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /exams/1 or /exams/1.json
   def update
-    respond_to do |format|
-      if @exam.update(exam_params)
-        format.html { redirect_to @exam, notice: "Exam was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @exam }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @exam.errors, status: :unprocessable_entity }
-      end
+    if @exam.update(exam_params)
+      fredirect_to @exam, notice: "Exam was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /exams/1 or /exams/1.json
   def destroy
     @exam.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to exams_path, notice: "Exam was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
+    redirect_to exams_path, notice: "Exam was successfully destroyed.", status: :see_other
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_exam
       @exam = Exam.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def exam_params
       params.expect(exam: [ :title, :description, :pass_grade ])
     end
--- app/views/exams/_exam.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! exam, :id, :title, :description, :pass_grade, :created_at, :updated_at
-json.url exam_url(exam, format: :json)
